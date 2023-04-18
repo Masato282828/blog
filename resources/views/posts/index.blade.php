6627379cc18c5f830cfc0ca23d1fbbec04376a80
Masato282828 <masato7010@icloud.com>
2023-04-18 18:10:49 +0900

    Curriculum8-3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,8 +10,8 @@
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                <div class='post'> 
+                    <a href="posts/{{ $post->id }}"> <h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
