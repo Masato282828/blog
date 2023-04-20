df1a117cf6c31c728b6adc48f1ea07a08c5c8f1d
Masato282828 <masato7010@icloud.com>
2023-04-20 20:02:20 +0900

    curricurum8-5

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -19,7 +19,7 @@
             <div class="body">
                 <h2>Body</h2>
                 <textarea name="post[body]" placeholder="今日も一日お疲れ様でした。">{{old('post.body')}}</textarea>
-                <p class='title_error' style="color:red">{{ $errors->first('post.body') }}</p>
+                <p class='body_error' style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="store">
         </form>
